3de32b33c107164f4dea2632c934a19283ca3c30
Drew-MyCoder <andrewsdwomoh9@gmail.com>
2024-04-17 07:06:27 +0000

    final project for a simple typescript project----clean city

--- typescript/src/App.tsx
+++ typescript/src/App.tsx
@@ -1,67 +1,53 @@
 import logo from './assets/sofa-logo.png'
 import './App.css'
 import './index.css'
-import { showReviewTotal, populateUser } from './pages/utils'
+import { showReviewTotal, populateUser, getTopTwoReviews } from './pages/utils'
+import { Review , Property} from './pages/interfaces'
+import { LoyaltyUser, Permissions } from './pages/enums'
+import MainProperty from './pages/classes'
 
 
 const propertyContainer = document.querySelector('.properties')
-let isOpen: boolean
+const reviewContainer = document.querySelector('.reviews')
+const container = document.querySelector('.container')
+const button = document.querySelector('button')
+const footer = document.querySelector('.footer')
 
-const reviews : { 
-  name: string; 
-  stars: number; 
-  loyaltyUser: boolean; 
-  date: string
-  }[] = [
+let isLoggedIn: boolean
+
+const reviews : Review[] = [
   {
       name: 'Sheia',
       stars: 5,
-      loyaltyUser: true,
+      loyaltyUser: LoyaltyUser.GOLD_USER,
       date: '01-04-2021'
   },
   {
       name: 'Andrzej',
       stars: 3,
-      loyaltyUser: false,
+      loyaltyUser: LoyaltyUser.BRONZE_USER,
       date: '28-03-2021'
   },
   {
       name: 'Omar',
       stars: 4,
-      loyaltyUser: true,
+      loyaltyUser: LoyaltyUser.SILVER_USER,
       date: '27-03-2021'
   },
 ]
 
 // User
-const you: {
-  firstName: string;
-  lastName: string;
-  isReturning: boolean;
-  age: number;
-  stayedAt: string[]
-} = {
+const you = {
   firstName: 'Bobby',
   lastName: 'Brown',
+  permissions: Permissions.ADMIN,
   isReturning: true,
   age: 35,
   stayedAt: ['florida-home', 'oman-flat', 'tokyo-bungalow']
 }
 
 // Array of Properties
-const properties : {
-  image: string;
-  title: string;
-  price: number;
-  location: {
-      firstLine: string;
-      city: string;
-      code: number;
-      country: string;
-  };
-  contact: [number, string];
-  isAvailable: boolean;
-}[] = [
+const properties : Property[] = [
   {
       image: '../src/assets/colombia-property.jpg',
       title: 'Colombian Shack',
@@ -78,7 +64,7 @@ const properties : {
   {
       image: '../src/assets/poland-property.jpg',
       title: 'Polish Cottage',
-      price: 34,
+      price: 30,
       location: {
           firstLine: 'no 23',
           city: 'Gdansk',
@@ -91,7 +77,7 @@ const properties : {
   {
       image: '../src/assets/london-property.jpg',
       title: 'London Flat',
-      price: 23,
+      price: 25,
       location: {
           firstLine: 'flat 15',
           city: 'London',
@@ -100,15 +86,30 @@ const properties : {
       },
       contact: [ +1123495082908, 'andyluger@aol.com'],
       isAvailable: true
-  }
+  },
+  {
+    image: '../src/assets/malaysian-hotel.jpeg',
+    title: 'Malia Hotel',
+    price: 35,
+    location: {
+        firstLine: 'Room 4',
+        city: 'Malia',
+        code: 45334,
+        country: 'Malaysia'
+    },
+    contact: [ +60349822083, 'lee34@gmail.com'],
+    isAvailable: false
+}
 ]
 
 
-
+// Functions
 showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser)
 
 populateUser(you.isReturning, you.firstName)
 
+
+// Add the properties
 for (let i = 0; i < properties.length; i++){
   const card = document.createElement('div')
   card.classList.add('card')
@@ -118,6 +119,42 @@ for (let i = 0; i < properties.length; i++){
   card.appendChild(image)
   propertyContainer?.appendChild(card)
 }
+
+let count = 0
+function addReviews(array : Review[]) : void {
+    if (!count ) {
+        count++
+        const topTwo = getTopTwoReviews(array)
+        for (let i = 0; i < topTwo.length; i++) {
+            const card = document.createElement('div')
+            card.classList.add('review-card')
+            card.innerHTML = topTwo[i].stars + ' stars from ' + topTwo[i].name
+            reviewContainer.appendChild(card)
+        }
+        container.removeChild(button) 
+    }
+}
+
+button.addEventListener('click', () => addReviews(reviews))
+
+let currentLocation: [string, string, number] = ['kumasi', '4:08', 35]
+
+footer.innerHTML = currentLocation[0] + ' ' + currentLocation[1] + ' ' + currentLocation[2] + '°'
+
+let yourMainProperty = new MainProperty(
+  '../src/assets/italian-property.jpg', 
+  'Italian House',
+  [{
+      name: 'Olive',
+      stars: 5,
+      loyaltyUser: LoyaltyUser.GOLD_USER,
+      date: '12-04-2021'
+  }] )
+
+const mainImageContainer = document.querySelector('.main-image')
+const image = document.createElement('img')
+image.setAttribute('src', yourMainProperty.src)
+mainImageContainer.appendChild(image)
 
 function App() {
   
